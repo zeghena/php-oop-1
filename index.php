70c554d06678e47ac4f1aadcd61f65c4feb458d6
davidemarchini <marchini73@gmail.com>
2024-03-04 17:53:04 +0100

    ....trying

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ class Production
     public $language;
 
     public $vote;
+
+
+    function __construct($title,$language,$vote)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+    }
     
     public function getname()
     {
@@ -22,19 +30,19 @@ $dexter->language = "English";
 $dexter->vote = "8";
 
 $braking_bad = new Production();
-$braking_bad->title = "braking_bad";
+$braking_bad->title = "Braking_Bad";
 $braking_bad->language = "Spanish";
 $braking_bad->vote = "9";
 
 $true_detective = new Production();
-$true_detective->title = "true_detective";
+$true_detective->title = "True_Detective";
 $true_detective->language = "English";
 $true_detective->vote = "10";
 
 $gomorra = new Production();
-$gomorra->title = "gomorra";
+$gomorra->title = "Gomorra";
 $gomorra->language = "Italian";
 $gomorra->vote = "9.5";
 
 
-var_dump($dexter->getname());
+var_dump($gomorra->getname());
