5bfef114fae50d927b491bd2917272c2d1c70516
davidemarchini <marchini73@gmail.com>
2024-03-04 17:06:28 +0100

    new productions

--- index.php
+++ index.php
@@ -10,7 +10,25 @@ class Production
     public $vote;
 }
 
-$Dexter = new Production();
-$Dexter->title = "Dexter";
-$Dexter->language = "English";
-$Dexter->vote = "9";
+$dexter = new Production();
+$dexter->title = "Dexter";
+$dexter->language = "English";
+$dexter->vote = "8";
+
+$braking_bad = new Production();
+$braking_bad->title = "braking_bad";
+$braking_bad->language = "Spanish";
+$braking_bad->vote = "9";
+
+$true_detective = new Production();
+$true_detective->title = "true_detective";
+$true_detective->language = "English";
+$true_detective->vote = "10";
+
+$gomorra = new Production();
+$gomorra->title = "gomorra";
+$gomorra->language = "Italian";
+$gomorra->vote = "9.5";
+
+
+var_dump($gomorra);
